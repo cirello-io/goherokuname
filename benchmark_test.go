4225b7af476c43159791f7985ee80ab2f7b4f778
Carlos C <uldericofilho@gmail.com>
2017-07-17 21:34:13 -0700

    All benchmarks use random number generator

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,24 +6,6 @@ import (
 	"cirello.io/goherokuname"
 )
 
-func BenchmarkHaikunateDeterministic(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		goherokuname.Haikunate()
-	}
-}
-
-func BenchmarkHaikunateHexDeterministic(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		goherokuname.HaikunateHex()
-	}
-}
-
-func BenchmarkHaikunateCustomDeterministic(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		goherokuname.HaikunateCustom("+", 5, "abcd")
-	}
-}
-
 func BenchmarkHaikunateRandom(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		goherokuname.Haikunate()
