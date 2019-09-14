b436bae8c2c5e0385a01d539bbab5adb6cf9af0b
Ulderico Cirello <u@cirello.eu>
2019-09-14 11:34:43 +0200

    goherokuname: remove lists with NSFW words

--- nouns_short.go
+++ nouns_short.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !herokuComplete,!herokuSimple
-
 package goherokuname
 
 var nouns = []string{
